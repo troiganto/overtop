d98836c6a4504f6c07f3541b5a1d175469eb5dde
Taschenschieber <stefh123@yahoo.de>
2013-11-01 01:49:05 +0100

    Implemented simple node.js server.
    No extra HTTP server is needed anymore.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+
+// Expose /assets, /scripts and /lib just like an ordinary webserver would do.
+app.use("/assets", express.static("assets"));
+app.use("/scripts", express.static("scripts"));
+app.use("/lib", express.static("lib"));
+
+// Give anybody navigating to our website root the game page.
+app.get("/", function(req,res){
+  res.sendfile("index.html");
+});
+
+// Start the server.
+app.listen(8888);
