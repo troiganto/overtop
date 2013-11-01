609c6abb16a4cb2bb48f041e817fb29f85e0dd60
Taschenschieber <stefh123@yahoo.de>
2013-11-01 02:53:34 +0100

    grunt now generates documentation

--- gruntfile.js
+++ gruntfile.js
@@ -10,13 +10,28 @@ module.exports = function(grunt) {
         outDir: "scripts",
         options: {}
       }
-    }
+    },
+            // The YUIDoc plugin to generate documentation for code files.
+        yuidoc: {
+            compile: {
+                name: 'Overtop',
+                description: '',
+                version: '0.1.0',
+                url: '',
+                options: {
+                    extension: '.ts',
+                    paths: 'scripts/',
+                    outdir: 'docs/'
+                }
+            }
+        }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
 
   // Default task(s).
-  grunt.registerTask('default', ['ts']);
+  grunt.registerTask('default', ['ts', 'yuidoc']);
 
 };
