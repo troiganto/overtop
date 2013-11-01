2311619e879ab6af1b473f74f309047ce021b038
Nico Madysa <Nico.Madysa@mailbox.tu-dresden.de>
2013-11-01 02:34:15 +0100

    Added a few test assets to play around with.

---
+++ assets/maps/Zelda Overworld Tileset.tsx
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Zelda Overworld Tileset" tilewidth="16" tileheight="16">
+ <image source="tileset.png" trans="ff00ff" width="80" height="176"/>
+ <terraintypes>
+  <terrain name="soil" tile="6"/>
+  <terrain name="tall grass" tile="30"/>
+  <terrain name="impassable" tile="35"/>
+  <terrain name="shallow water" tile="48"/>
+  <terrain name="deep water" tile="41"/>
+  <terrain name="interactive" tile="52"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,0,0,0"/>
+ <tile id="12" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="2,2,2,2">
+  <properties>
+   <property name="left" value="True"/>
+   <property name="top" value="True"/>
+  </properties>
+ </tile>
+ <tile id="19" terrain="2,2,2,2">
+  <properties>
+   <property name="right" value="True"/>
+   <property name="top" value="True"/>
+  </properties>
+ </tile>
+ <tile id="20" terrain="0,0,0,0"/>
+ <tile id="21" terrain="0,0,0,0"/>
+ <tile id="22" terrain="0,0,0,0"/>
+ <tile id="23" terrain="2,2,2,2">
+  <properties>
+   <property name="bottom" value="True"/>
+   <property name="left" value="True"/>
+  </properties>
+ </tile>
+ <tile id="24" terrain="2,2,2,2">
+  <properties>
+   <property name="bottom" value="True"/>
+   <property name="right" value="True"/>
+  </properties>
+ </tile>
+ <tile id="25" terrain="0,0,0,0"/>
+ <tile id="26" terrain="0,0,0,0"/>
+ <tile id="27" terrain="0,0,0,0"/>
+ <tile id="28" terrain="2,2,2,2"/>
+ <tile id="29" terrain="2,2,2,2"/>
+ <tile id="30" terrain="1,1,1,1"/>
+ <tile id="31" terrain="0,0,0,0"/>
+ <tile id="32" terrain="0,0,0,0"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="5,5,5,5"/>
+ <tile id="35" terrain="2,2,2,2"/>
+ <tile id="36" terrain="2,2,2,2">
+  <properties>
+   <property name="bottom" value="True"/>
+  </properties>
+ </tile>
+ <tile id="37" terrain="2,2,2,2"/>
+ <tile id="38" terrain="2,2,2,2"/>
+ <tile id="39" terrain="2,2,2,2"/>
+ <tile id="40" terrain="2,2,2,2">
+  <properties>
+   <property name="right" value="True"/>
+  </properties>
+ </tile>
+ <tile id="41" terrain="4,4,4,4"/>
+ <tile id="42" terrain="2,2,2,2">
+  <properties>
+   <property name="left" value="True"/>
+  </properties>
+ </tile>
+ <tile id="43" terrain="2,2,2,2"/>
+ <tile id="44" terrain="2,2,2,2"/>
+ <tile id="45" terrain="2,2,2,2"/>
+ <tile id="46" terrain="2,2,2,2">
+  <properties>
+   <property name="top" value="True"/>
+  </properties>
+ </tile>
+ <tile id="47" terrain="2,2,2,2"/>
+ <tile id="48" terrain="3,3,3,3"/>
+ <tile id="49" terrain="0,0,0,0"/>
+ <tile id="50" terrain="5,5,5,5"/>
+ <tile id="51" terrain="5,5,5,5"/>
+ <tile id="52" terrain="5,5,5,5"/>
+ <tile id="53" terrain="2,2,2,2">
+  <properties>
+   <property name="bottom" value="True"/>
+   <property name="left" value="True"/>
+   <property name="right" value="True"/>
+   <property name="top" value="True"/>
+  </properties>
+ </tile>
+ <tile id="54" terrain="5,5,5,5"/>
+</tileset>
